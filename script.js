b06047412864b763b1e15b67ca0b2e63301f8135
saptarshi418 <106703507+saptarshi418@users.noreply.github.com>
2023-10-01 01:28:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let nav_toggle = document.querySelectorAll(".nav_toggle");
+let sidebar = document.querySelector(".sidebar");
+
+
+
+nav_toggle.addEventListener("click",function(){
+    // nav_toggle.firstChild.classList.replace("fa-bars","fa-times");
+    console.log("clicked");
+})
